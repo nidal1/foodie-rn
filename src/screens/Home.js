d8287f40a8d6af10cc2016bf65f73fca26dca072
nidal1 <touhami9@gmail.com>
2023-04-03 22:33:47 +0000

    Add Home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,64 @@
-import { View, Text } from "react-native";
+import { Text, View, Image } from "react-native";
+import { Button } from "native-base";
 import React from "react";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
+const Home = (props) => {
+  const { navigation } = props;
+  return (
+    <View className="bg-primary-2 flex-1 pb-[53px]">
+      <Image
+        source={require("../../assets/images/Illustration_img.png")}
+        alt="Alternate Text"
+      />
 
-const Home = () => {
-  return <View className="bg-primary-1 flex-1"></View>;
+      <View className="z-20 justify-center items-center flex-1">
+        <View className="justify-between flex-1 w-full px-6">
+          <View className="z-20 justify-center items-center">
+            <View className="flex-row gap-2">
+              <Text className="font-montserrat font-semibold text-[34px] text-[#222B45] mb-5 leading-[41px]">
+                Gluten
+              </Text>
+              <Text className="font-montserrat font-semibold text-[34px] text-[#222B45] mb-5 leading-[41px] border-b-2 border-[#DB9F01]">
+                Free
+              </Text>
+            </View>
+
+            <Text className="font-montserrat font-semibold text-base text-secondary-2 text-center">
+              We Help You Find Gluten Free Food
+            </Text>
+            <Text className="font-montserrat font-semibold text-base text-secondary-2 text-center">
+              Around You.
+            </Text>
+          </View>
+
+          <View className="gap-[20px]">
+            <Button
+              className="p-[17px] rounded-full bg-white"
+              onPress={() => navigation.navigate("Maps")}
+            >
+              <View className="flex-row gap-2 items-center">
+                <EvilIcons name="sc-facebook" size={24} color="black" />
+                <Text className="font-montserrat font-semibold text-base text-secondary-1">
+                  Login with Facebook
+                </Text>
+              </View>
+            </Button>
+            <Button
+              className="p-[17px] rounded-full bg-white"
+              onPress={() => navigation.navigate("Maps")}
+            >
+              <View className="flex-row gap-2 items-center">
+                <EvilIcons name="sc-google-plus" size={24} color="black" />
+                <Text className="font-montserrat font-semibold text-base text-secondary-1">
+                  Login with Google
+                </Text>
+              </View>
+            </Button>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
+// const { Font } = require("expo");
+// const loadFont = async () => {
+//   await Font.loadAsync({
+//     Montserrat: require("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Work+Sans:ital,wght@0,400;1,600&display=swap"),
+//   });
+// };
+// loadFont();
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-1": "#FEBD11",
+        "primary-2": "#FEC633",
+        "secondary-1": "#131313",
+        "secondary-2": "#4E4E4E",
+        placeholder: "#8F9BB3",
+        muted: "#F3F3F3",
+        bright: "#FFFFFF",
+      },
+      fontFamily: {
+        montserrat: "'Montserrat', sans-serif",
+        worksans: "'Work Sans', sans-serif",
+      },
+    },
   },
   plugins: [],
 };
